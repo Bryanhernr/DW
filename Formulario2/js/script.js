5d55b2c7662ec19437cd3191f85461f67a145cfe
ESCCOD10 <vmiranda_99@yahoo.com>
2023-06-01 11:46:54 -0600

    Clase jueves

---
+++ Formulario2/js/script.js
@@ -0,0 +1,16 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+const nombre = urlParams.get("nombre");
+const apellido_p = urlParams.get("apellido_p");
+const apellido_m = urlParams.get("apellido_m")
+const correo = urlParams.get("correo")
+const descripcion = urlParams.get("descripcion")
+const estado = urlParams.get("estado")
+
+document.getElementById("nombre_home_1").textContent = nombre;
+document.getElementById("nombre_home_2").textContent = nombre;
+document.getElementById("apellido_p").textContent = apellido_p+" ";
+document.getElementById("apellido_m").textContent = apellido_m;
+document.getElementById("correo").textContent = correo;
+document.getElementById("descripcion").textContent = descripcion;
+document.getElementById("estado").textContent = estado;
